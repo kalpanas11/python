07a305c440da220eb46e776b57e1619c54932f0e
Kalpanas11 <pskalpana@yahoo.com>
2024-03-22 00:49:56 +0100

    Add untracked folders

---
+++ simplecrud/srcFiles/helloworld.py
@@ -0,0 +1,16 @@
+
+def hello_World():
+#    print("hi...")
+     return ("Hello World")
+
+
+print("hi...")
+
+print(hello_World())
+
+# Define a function
+def greet(name):
+    print("Hello,", name)
+
+# Call the function
+greet("Alice")
---
+++ simplecrud/test/test_helloWorld.py
@@ -0,0 +1,5 @@
+from srcFiles.helloworld import hello_World
+
+def test_hello_world():
+    assert "Hello World" == hello_World()
+
