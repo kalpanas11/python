077a455ef6a10ce13c6b169aa2b2520b049dda5a
Kalpanas11 <pskalpana@yahoo.com>
2024-03-26 00:01:52 +0100

    Added 'firstDockerTest' directory

---
+++ firstDockerTest/helloworld.py
@@ -0,0 +1,13 @@
+
+def hello_World():
+#    print("hi...")
+     return ("Hello World!!")
+
+print(hello_World())
+
+# Define a function
+def greet(name):
+    print("Hello,", name)
+
+# Call the function
+greet("Alice...")
